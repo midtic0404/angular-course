016a8f464fe9d1a1c7e6afa08c6f7fd3b05fb414
cchechang <cchechang@mastercontrol.com>
2020-10-22 14:51:24 -0600

    finished auth section

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { AuthGuard } from './auth/auth.guard';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RecipesComponent } from './recipes/recipes.component';
@@ -10,12 +11,15 @@ import { AuthComponent } from './auth/auth.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/recipes', pathMatch: 'full' },
-  { path: 'recipes', component: RecipesComponent, children: [
-    { path: '', component: RecipeStartComponent},
-    { path: 'new', component: RecipeEditComponent },
-    { path: ':id', component: RecipeDetailComponent, resolve: [RecipesResolverService]},
-    { path: ':id/edit', component: RecipeEditComponent, resolve: [RecipesResolverService] },
-  ] },
+  { path: 'recipes', component: RecipesComponent,
+    canActivate: [AuthGuard],
+    children: [
+      { path: '', component: RecipeStartComponent},
+      { path: 'new', component: RecipeEditComponent },
+      { path: ':id', component: RecipeDetailComponent, resolve: [RecipesResolverService]},
+      { path: ':id/edit', component: RecipeEditComponent, resolve: [RecipesResolverService] },
+    ]
+  },
   { path: 'shopping-list', component: ShoppingListComponent },
   { path: 'auth', component: AuthComponent },
 ];
